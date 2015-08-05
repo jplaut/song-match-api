f7e247cc213a205eb3cacbd0beffb2a060b05e32
Jon Plaut <jon@brightbytes.net>
2015-08-05 00:00:06 -0700

    move secret key to env var

--- __init__.py
+++ __init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask.ext.mongoengine import MongoEngine
+import os
 
 app = Flask(__name__)
 app.config["MONGODB_SETTINGS"] = {'DB': "song_match"}
-app.config["SECRET_KEY"] = "secret"
+app.config["SECRET_KEY"] = os.getenv('SECRET_KEY')
 
 db = MongoEngine(app)
 
